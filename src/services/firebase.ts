7cb2e7d022fd3e5ca7880ab9e99c6ea1a82fdcd4
Breno Henrique <bhfs.2908@gmail.com>
2021-08-03 11:50:14 -0300

    fix:firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAeBcF-vtMkV9eeYqLp-oy6xDASwV4pdzY",
-    authDomain: "letmeask-9b328.firebaseapp.com",
-    databaseURL: "https://letmeask-9b328-default-rtdb.firebaseio.com",
-    projectId: "letmeask-9b328",
-    storageBucket: "letmeask-9b328.appspot.com",
-    messagingSenderId: "377173251259",
-    appId: "1:377173251259:web:1ea43e0cf559bbc4dc859e"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig); 
