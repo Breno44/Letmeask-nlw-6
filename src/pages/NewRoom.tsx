7817c1c8bd536afd79058f720245587a37e17a01
Breno Henrique <bhfs.2908@gmail.com>
2021-08-03 16:23:12 -0300

    feat:auth

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,8 +4,11 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.css';
 import { Button } from '../components/Button';
+// import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
+    // const { user } = useAuth();
+
     return (
         <div id="page-auth">
             <aside>
