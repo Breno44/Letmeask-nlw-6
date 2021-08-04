2c6508a72a16308d58af6f2d1c0a22a0380d7a1f
Breno Henrique <bhfs.2908@gmail.com>
2021-08-04 17:58:52 -0300

    feat:add room

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 import { AuthContextProvider  } from './contexts/AuthContext'
+import { Room } from './pages/Room';
 
 function App() {
 
@@ -11,8 +12,11 @@ function App() {
   return (
       <BrowserRouter>
         <AuthContextProvider>
+          <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/rooms/new" component={NewRoom} />
+            <Route path="/rooms/:id" component={Room} />
+          </Switch>
         </AuthContextProvider>
       </BrowserRouter>
   );
